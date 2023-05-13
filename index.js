bb887414c4abe3c2224ba15bd6197af6115803ea
Roman Kopylov <romankopilov1712@gmail.com>
2023-05-13 16:38:52 +0300

    add img files

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 const express = require('express');
 const cors = require('cors');
+const multer = require('multer');
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 app.use(cors());
 
-app.post('/api/data', (req, res) => {
-  console.log(req.body);
-  res.send('Received data');
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, `${Date.now()}_${file.originalname}`);
+  }
+});
+
+const upload = multer({ storage: storage }).single("image");
+
+app.post('/api/data', upload, (req, res) => {
+  if (req.file) {
+    console.log(req.file);
+    res.send('Received image');
+  } else {
+    console.log(req.body);
+    res.send('Received data');
+  }
 });
 
 app.listen(port, () => {
